f8d84f2daa37f1621506779636a0b5b57c0349b4
LiDong <ldwqh0@outlook.com>
2020-05-27 16:29:51 +0800

    重新设计页面

--- web/src/components/svc/svc.vue
+++ web/src/components/svc/svc.vue
@@ -1,112 +1,118 @@
 <template>
-  <div>
-    <el-form ref="svcForm" label-width="100px">
-      <el-row>
-        <el-col :span="24">服务信息</el-col>
-      </el-row>
-      <el-row>
-        <el-col :span="12">
-          <el-form-item prop="name" label="服务名称">
-            <el-input v-model="svc.name" />
-          </el-form-item>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="12">
-          <el-form-item prop="connectionId" label="服务连接">
-            <el-select v-model="svc.connectionId" @change="getMeta()">
-              <el-option :label="cnn.name"
-                         :value="cnn.id"
-                         :key="cnn.name"
-                         v-for="cnn in cnns">
-                <span>{{ cnn.name }}</span>
-              </el-option>
-            </el-select>
-          </el-form-item>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="12">
-          <el-form-item label="描述">
-            <el-input type="textarea" v-model="svc.description" />
-          </el-form-item>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="12">
-          <el-form-item prop="sql" label="SQL语句">
-            <el-input type="textarea" v-model="svc.sql" @blur="getMeta()" />
-          </el-form-item>
-        </el-col>
-      </el-row>
-      <el-form-item />
-      <div>
+  <el-form ref="svcForm" label-width="100px">
+    <el-collapse v-model="active">
+      <el-collapse-item name="1">
+        <template v-slot:title>
+          <h1>服务信息</h1>
+        </template>
         <el-row>
-          <el-col :span="24">条件列</el-col>
-        </el-row>
-        <el-row v-for="(parameter,index) in svc.parameters" :key="index">
-          <!--<el-col>{{ parameter }},{{ index }}</el-col>-->
-          <el-col :span="5">{{ parameter.column }}</el-col>
-          <el-col :span="5">
-            <el-select v-model="parameter.operator">
-              <el-option value="=" />
-              <el-option value=">" />
-              <el-option value="<" />
-              <el-option value=">=" />
-              <el-option value="<=" />
-              <el-option value="!=" />
-              <el-option label="部分包含" value="like" />
-              <el-option label="选项之一" value="in" />
-            </el-select>
+          <el-col :span="12">
+            <el-form-item prop="name" label="服务名称">
+              <el-input v-model="svc.name" />
+            </el-form-item>
           </el-col>
-          <el-col :span="5">
-            <el-input v-model="parameter.parameterName" />
-          </el-col>
-          <el-col :span="5">
-            <a href="javascript:;" @click="removeParameter(index)">移除</a>
+          <el-col :span="12">
+            <el-form-item prop="connectionId" label="服务连接">
+              <el-select v-model="svc.connectionId" @change="getMeta()">
+                <el-option :label="cnn.name"
+                           :value="cnn.id"
+                           :key="cnn.name"
+                           v-for="cnn in cnns">
+                  <span>{{ cnn.name }}</span>
+                </el-option>
+              </el-select>
+            </el-form-item>
           </el-col>
         </el-row>
-      </div>
 
-      <div>
         <el-row>
-          <el-col :span="24">排序列</el-col>
-        </el-row>
-        <el-row v-for="(order,index) in svc.orders" :key="index">
-          <el-col :span="5">{{ order.column }}</el-col>
-          <el-col :span="10">
-            <el-select v-model="order.direction">
-              <el-option label="升序" value="ASC" />
-              <el-option label="降序" value="DESC" />
-            </el-select>
-          </el-col>
-          <el-col :span="5">
-            <a href="javascript:;" @click="removeOrder(index)">移除</a>
+          <el-col>
+            <el-form-item label="描述">
+              <el-input type="textarea" v-model="svc.description" />
+            </el-form-item>
           </el-col>
         </el-row>
-      </div>
 
-      <el-row>
-        <el-col :span="24">
-          <el-button type="primary" @click="submit()">提交</el-button>
-          <el-button type="danger" @click="goback()">取消</el-button>
-        </el-col>
-      </el-row>
-    </el-form>
+        <el-row>
+          <el-col>
+            <el-form-item prop="sql" label="SQL语句">
+              <el-input type="textarea" v-model="svc.sql" @blur="getMeta()" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-collapse-item>
 
-    <el-row>
-      <el-form>
+      <el-collapse-item name="2">
+        <template v-slot:title>
+          <h1>调解参数</h1>
+        </template>
+        <ele-data-tables :data="svc.parameters">
+          <el-table-column prop="column" label="列名称" />
+          <el-table-column label="比较符">
+            <template v-slot="{ row }">
+              <el-form-item label-width="0px">
+                <el-select v-model="row.operator">
+                  <el-option value="=" />
+                  <el-option value=">" />
+                  <el-option value="<" />
+                  <el-option value=">=" />
+                  <el-option value="<=" />
+                  <el-option value="!=" />
+                  <el-option label="部分包含" value="like" />
+                  <el-option label="选项之一" value="in" />
+                </el-select>
+              </el-form-item>
+            </template>
+          </el-table-column>
+          <el-table-column label="参数名称">
+            <template v-slot="{row}">
+              <el-form-item label-width="0px">
+                <el-input v-model="row.parameterName" />
+              </el-form-item>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作">
+            <template v-slot="{row}">
+              <a href="javascript:;" @click="{{removeParameter(row)}}">移除</a>
+            </template>
+          </el-table-column>
+        </ele-data-tables>
+      </el-collapse-item>
+      <el-collapse-item name="3">
+        <template v-slot:title>
+          <h1>排序列</h1>
+        </template>
+
+        <ele-data-tables :data="svc.orders">
+          <el-table-column prop="column" label="列名称"></el-table-column>
+          <el-table-column label="排序方式">
+            <template v-slot="{row}">
+              <el-form-item label-width="0px">
+                <el-select v-model="row.direction" style="width:100%;">
+                  <el-option label="升序" value="ASC" />
+                  <el-option label="降序" value="DESC" />
+                </el-select>
+              </el-form-item>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作">
+            <template v-slot="{row}">
+              <a href="javascript:;" @click="removeOrder(row)">移除</a>
+            </template>
+          </el-table-column>
+        </ele-data-tables>
+      </el-collapse-item>
+
+      <el-collapse-item name="4">
+        <template v-slot:title>
+          <h1>输出字段</h1>
+        </template>
         <ele-data-tables :data="columns">
-          <el-table-column prop="columnName" label="列名称" />
+          <el-table-column prop="columnLabel" label="列名称" />
           <el-table-column prop="columnTypeName" label="数据类型" />
           <el-table-column label="说明">
-            <template v-slot="{row:{columnName}}">
-              <el-form-item>
-                <el-input v-model="svc.columns[columnName]" />
-              </el-form-item>
+            <template v-slot="{row:{columnLabel}}">
+              <el-input v-model="svc.columns[columnLabel]" />
             </template>
           </el-table-column>
           <el-table-column label="操作">
@@ -116,9 +122,15 @@
             </template>
           </el-table-column>
         </ele-data-tables>
-      </el-form>
+      </el-collapse-item>
+    </el-collapse>
+    <el-row>
+      <el-col :span="24">
+        <el-button type="primary" @click="submit()">提交</el-button>
+        <el-button type="danger" @click="goback()">取消</el-button>
+      </el-col>
     </el-row>
-  </div>
+  </el-form>
 </template>
 <script>
   import Vue from 'vue'
@@ -134,6 +146,8 @@
   export default class SvcForm extends Vue {
     @Prop({ default: 'new' })
     id
+
+    active = ['1', '2', '3', '4']
 
     columns = []
     cnns = []
@@ -177,9 +191,9 @@
         this.$set(this.svc, 'parameters', [])
       }
       this.svc.parameters.push({
-        column: row.columnName,
+        column: row.columnLabel,
         operator: '=',
-        parameterName: row.columnName
+        parameterName: row.columnLabel
       })
     }
 
@@ -189,16 +203,22 @@
         this.$set(this.svc, 'orders', [])
       }
       this.svc.orders.push({
-        column: row.columnName,
+        column: row.columnLabel,
         direction: 'ASC'
       })
     }
 
-    removeOrder (index) {
+    vis (obj) {
+      return obj.row
+    }
+
+    removeOrder (row) {
+      const index = this.svc.orders.findIndex(item => item === row)
       this.svc.orders.splice(index, 1)
     }
 
-    removeParameter (index) {
+    removeParameter (row) {
+      const index = this.svc.parameters.findIndex(item => item === row)
       this.svc.parameters.splice(index, 1)
     }
 
